f79845145cd29cbbb1f8801f93bc69df73519840
Gabby <gabbie10112005@gmail.com>
2025-01-24 14:12:59 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace References_Miller_Gabriella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sedan myFirstSedan = new Sedan(0);
+            IAutomobile myAutomobile = myFirstSedan;
+            Sedan myOtherSedan = new Sedan(0);
+
+
+            myFirstSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myAutomobile.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+
+
+            myOtherSedan.IncreaseSpeed();
+            Console.WriteLine(myFirstSedan.Speed);
+            Console.WriteLine(myOtherSedan.Speed);
+            Console.WriteLine(myFirstSedan.Equals(myOtherSedan));
+
+            Truck myTruck = new Truck(50, 500, "MyTrUck");
+
+            myFirstSedan.Stringify();
+            myAutomobile.Stringify();
+            myOtherSedan.Stringify();
+            myTruck.Stringify();
+          
+        }
+    }
+}
---
+++ Truck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_Miller_Gabriella
+{
+    internal class Truck : IAutomobile
+    {
+        public double Speed { get; private set; }
+
+        public int Wheels { get; private set; }
+
+        public string LicensePlate { get; private set; }
+
+        public double Weight { get; }
+
+        public Truck(double speed, double weight, string licenseNum)
+        {
+            Speed = speed;
+            Weight = weight;
+            LicensePlate = licenseNum;
+
+            if(Weight < 400)
+            {
+                Wheels = 8;
+            } else
+            {
+                Wheels = 12;
+            }
+        }
+
+        public void Stringify()
+        {
+             Console.WriteLine($"The Truck is travelling at a speed of {Speed} on {Wheels} wheels, with a License Plate # of {LicensePlate}!");
+        }
+
+        public void IncreaseSpeed()
+        {
+
+            Speed += 5;
+
+        }
+        public void DecreaseSpeed()
+        {
+
+            Speed -= 5;
+
+        }
+    }
+}
